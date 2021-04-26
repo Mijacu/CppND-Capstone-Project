5093ec67dc8c0293c7efb3e786cc25e10b501040
root <mijacu_018@hotmail.com>
2021-04-26 03:36:39 +0000

    Commmented out main file

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 #include "CircularTrajectory.h"
 
 int main() {
+    // Initialiaze a 2 DOF robotic arm
     std::vector<double> links {10.0, 5.0};
     RoboticArm robotic_arm(links);
 
-    Position position(1.0, 2.0, 4.0);
-    position.print("position: ");
-
-    arma::mat postion = robotic_arm.computePosition();
+    // Compute initial position
+    arma::mat position = robotic_arm.computePosition();
     position.print("Computed positions: ");
 
+    // Test inverse kinematics on two desired positions
     std::vector<double> desired_position{0.0, 15.0};
     robotic_arm.computeAngles(desired_position);
     robotic_arm.printAngles();
@@ -24,12 +24,14 @@ int main() {
     robotic_arm.computeAngles(desired_position);
     robotic_arm.printAngles();
 
+    // Generate a circular trajectory and calculate the angles to carry out that trajectory
     CircularTrajectory circular_trajectory;
     std::vector<std::vector<double>> trajectory = circular_trajectory.generateTrajectory();
     for (std::vector<double> position : trajectory) {
         robotic_arm.computeAngles(position);
+        std::cout << "x= " << position[0] << " y= " << position[1] << std::endl;
         robotic_arm.printAngles();
     }
-    
+
     return 0;
 }
